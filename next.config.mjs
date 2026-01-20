db439413285afb5ce1b965361c8aadbb21a01a0e
Aditya Khote <21csme004aditya@gmail.com>
2026-01-21 01:05:51 +0530

    Ignore ESLint during build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
